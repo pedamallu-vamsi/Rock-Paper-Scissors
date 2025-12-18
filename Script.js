3fc38a6cbb92d13e63b661a9eb3bb8004d790b24
pedamallu-vamsi <vamsitkd@gmail.com>
2025-12-18 15:38:30 +0530

    Added win count and loss count inside the popup

--- Script.js
+++ Script.js
@@ -1,38 +1,54 @@
+var wcount = 0;
+var lcount = 0;
+
 function buttonRock(){
-    var r=Math.floor(Math.random()*3);
-    if(r==0){
-        showPopup("The computer choose rock, Its a Tie!!");
+    var r = Math.floor(Math.random() * 3);
+    if(r == 0){
+        showPopup("The computer chose Rock. It's a Tie!!");
     }
-    else if(r==1){
-        showPopup("The computer choose paper, You Lose!!");
-    }else{
-        showPopup("The computer chooses scissors, You Win!!");
+    else if(r == 1){
+        lcount++;
+        showPopup("The computer chose Paper. You Lose!!");
+    }
+    else{
+        wcount++;
+        showPopup("The computer chose Scissors. You Win!!");
     }
 }
+
 function buttonPaper(){
-    var r=Math.floor(Math.random()*3);
-    if(r==0){
-        showPopup("The computer choose rock, You Win!!");
+    var r = Math.floor(Math.random() * 3);
+    if(r == 0){
+        wcount++;
+        showPopup("The computer chose Rock. You Win!!");
     }
-    else if(r==1){
-        showPopup("The computer choose paper, It's a Tie!!");
-    }else{
-        showPopup("The computer chooses scissors, You Lose!!");
+    else if(r == 1){
+        showPopup("The computer chose Paper. It's a Tie!!");
+    }
+    else{
+        lcount++;
+        showPopup("The computer chose Scissors. You Lose!!");
     }
 }
+
 function buttonScissor(){
-    var r=Math.floor(Math.random()*3);
-    if(r==0){
-        showPopup("The computer choose rock, You Lose!!");
+    var r = Math.floor(Math.random() * 3);
+    if(r == 0){
+        lcount++;
+        showPopup("The computer chose Rock. You Lose!!");
     }
-    else if(r==1){
-        showPopup("The computer choose paper, You Win!!");
-    }else{
-        showPopup("The computer chooses scissors, It's a Tie!!");
+    else if(r == 1){
+        wcount++;
+        showPopup("The computer chose Paper. You Win!!");
+    }
+    else{
+        showPopup("The computer chose Scissors. It's a Tie!!");
     }
 }
+
 function showPopup(message) {
-    document.getElementById("resultText").innerText = message;
+    document.getElementById("resultText").innerHTML =
+        message + "<br><br>Wins = " + wcount + "<br>Losses = " + lcount;
     document.getElementById("popup").style.display = "flex";
 }
 
